76eae69b7f95e7cf2cf7159c4c3e79a8bcddfc71
Entorrey <107317621+Entorrey@users.noreply.github.com>
2022-07-03 18:01:03 -0400

    Add files via upload

---
+++ P4Lab2a_PattersonErica.py
@@ -0,0 +1,13 @@
+import turtle
+
+my_turtle= turtle.Turtle()
+my_turtle.shape('turtle')
+drawing_area= turtle.Screen()
+
+for i in range(4):
+           my_turtle.forward(200)
+           my_turtle.left(90)
+
+for i in range(3):
+    turtle.forward(300)
+    turtle.left(120)
